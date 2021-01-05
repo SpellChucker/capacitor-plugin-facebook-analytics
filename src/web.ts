709915e5dd5160ab2a3fee65af5813493e1ac12f
Guy Kedar <kedarguy@gmail.com>
2021-01-05 14:38:57 +0200

    support facebook major standard events

--- src/web.ts
+++ src/web.ts
@@ -13,6 +13,26 @@ export class FacebookAnalyticsWeb extends WebPlugin implements FacebookAnalytics
     // TODO: Implement.
     return Promise.resolve(options.event);
   }
+  logPurchase(options: { amount: number, params?: any, currency: string }): Promise<string> {
+    // TODO: Implement.
+    return Promise.resolve(JSON.stringify(options));
+  }
+  logAddPaymentInfo(options: {success: number}): Promise<string> {
+    // TODO: Implement.
+    return Promise.resolve(JSON.stringify(options));
+  }
+  logAddToCart(options: {amount: number, currency: string, params?: any}): Promise<string> {
+    // TODO: Implement.
+    return Promise.resolve(JSON.stringify(options));
+  }
+  logCompleteRegistration(options: {params?: any}): Promise<string> {
+    // TODO: Implement.
+    return Promise.resolve(JSON.stringify(options));
+  }
+  logInitiatedCheckout(options: {amount: number, params?: any}): Promise<string> {
+    // TODO: Implement.
+    return Promise.resolve(JSON.stringify(options));
+  }
 }
 
 const FacebookAnalytics = new FacebookAnalyticsWeb();
